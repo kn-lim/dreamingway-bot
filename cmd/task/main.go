48d0f904f37738c2a5919fa23d3c34544dc882e5
Kevin Lim <knlim95@gmail.com>
2023-10-05 16:44:58 -0700

    Separate sending deferred message

--- cmd/task/main.go
+++ cmd/task/main.go
@@ -1,62 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/bwmarrin/discordgo"
+
 	"github.com/kn-lim/dreamingway-bot/internal/discord"
 )
 
 func handler(interaction discordgo.Interaction) error {
-	log.Printf("Received interaction: %+v\n", interaction)
+	// log.Printf("Received interaction: %+v\n", interaction)
 
-	application_id := interaction.AppID
-	interaction_token := interaction.Token
-
-	url := fmt.Sprintf("%v/v%v/webhooks/%v/%v", discord.DiscordBaseURL, os.Getenv("DISCORD_API_VERSION"), application_id, interaction_token)
-
-	log.Printf("Discord API URL: %s", url)
-
-	payloadBytes, err := json.Marshal(map[string]string{
-		"content": "This is from the task lambda!",
-	})
-	if err != nil {
-		log.Fatalf("Error! Couldn't marshal JSON: %v", err)
-	}
-
-	log.Printf("Sending payload: %s", string(payloadBytes))
-
-	request, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
-	if err != nil {
-		log.Fatalf("Error! Couldn't create http request: %v", err)
-	}
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Authorization", "Bot "+os.Getenv("DISCORD_BOT_TOKEN"))
-
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		log.Fatalf("Error! Couldn't send the http request: %v", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		var result map[string]interface{}
-		if err := json.NewDecoder(response.Body).Decode(&result); err != nil {
-			log.Printf("Error! Couldn't decode result: %v", err)
-			return err
-		}
-		log.Printf("Error! Discord API Error: %v", result)
-		return fmt.Errorf("discord API error: %v", result)
-	}
-
-	return nil
+	return discord.SendDeferredMessage(interaction.AppID, interaction.Token, "This is from the task lambda!")
 }
 
 func main() {
--- internal/discord/utils.go
+++ internal/discord/utils.go
@@ -1,6 +1,15 @@
 package discord
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 const DiscordBaseURL = "https://discord.com/api"
 
@@ -8,4 +17,44 @@ func DeferredMessage(i *discordgo.Interaction) (discordgo.InteractionResponse, e
 	return discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
 	}, nil
+}
+
+func SendDeferredMessage(appID string, token string, content string) error {
+	log.Printf("Sending message: %s", content)
+
+	payload, err := json.Marshal(map[string]string{
+		"content": content,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't marshal JSON: %v", err)
+	}
+
+	url := fmt.Sprintf("%v/v%v/webhooks/%v/%v", DiscordBaseURL, os.Getenv("DISCORD_API_VERSION"), appID, token)
+	// log.Printf("Discord API URL: %s", url)
+
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return fmt.Errorf("couldn't create http request: %v", err)
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bot "+os.Getenv("DISCORD_BOT_TOKEN"))
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("couldn't send the http request: %v", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		var result map[string]interface{}
+		if err := json.NewDecoder(response.Body).Decode(&result); err != nil {
+			log.Printf("Error! Couldn't decode result: %v", err)
+			return err
+		}
+		log.Printf("Error! Discord API Error: %v", result)
+		return fmt.Errorf("discord API error: %v", result)
+	}
+
+	return nil
 }
