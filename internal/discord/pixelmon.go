386fe8dd50be75e4957550272651160596f40052
Kevin Lim <knlim95@gmail.com>
2023-10-07 00:40:12 -0700

    Implement more commands

--- internal/discord/pixelmon.go
+++ internal/discord/pixelmon.go
@@ -3,16 +3,21 @@ package discord
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
 
 	"github.com/kn-lim/dreamingway-bot/internal/pixelmon"
 )
 
+var (
+	ServerURL = fmt.Sprintf("%v.%v", os.Getenv("PIXELMON_SUBDOMAIN"), os.Getenv("PIXELMON_DOMAIN"))
+)
+
 func status(i *discordgo.Interaction) (string, error) {
 	log.Println("/pixelmon status")
 
-	result, online, err := pixelmon.GetStatus()
+	result, online, err := pixelmon.GetStatus(ServerURL)
 	if err != nil {
 		log.Printf("Error! Couldn't get status: %s", err)
 		return "", err
@@ -20,53 +25,141 @@ func status(i *discordgo.Interaction) (string, error) {
 
 	if result {
 		// log.Printf("%v is online", serverURL)
-		return fmt.Sprintf(":green_circle:   %s | Online | Number of Online Players: %v", pixelmon.ServerURL, online), nil
+		return fmt.Sprintf(":green_circle:   %s | Online | Number of Online Players: %v", ServerURL, online), nil
 	} else {
 		// log.Printf("%v is offline", serverURL)
-		return fmt.Sprintf(":red_circle:   %s | Offline", pixelmon.ServerURL), nil
+		return fmt.Sprintf(":red_circle:   %s | Offline", ServerURL), nil
 	}
 }
 
 func start(i *discordgo.Interaction) (string, error) {
 	log.Println("/pixelmon start")
 
-	if err := SendDeferredMessage(i.AppID, i.Token, fmt.Sprintf(":green_square:   %s | Starting the Pixelmon server", pixelmon.ServerURL)); err != nil {
+	// Check if user has correct role
+	verified := false
+	for _, role := range i.Member.Roles {
+		if role == os.Getenv("PIXELMON_ROLE_ID") {
+			verified = true
+			break
+		}
+	}
+	if !verified {
+		return "You don't have the required role to use this command!", nil
+	}
+
+	// Check if service is already running
+	status, _, err := pixelmon.GetStatus(ServerURL)
+	if err != nil {
+		return "", err
+	}
+	if status {
+		return fmt.Sprintf(":green_circle:   %s | Online", ServerURL), nil
+	}
+
+	if err := SendDeferredMessage(i.AppID, i.Token, fmt.Sprintf(":green_square:   %s | Starting the Pixelmon server", ServerURL)); err != nil {
 		return "", err
 	}
 
-	if err := pixelmon.StartInstance(); err != nil {
+	if err := pixelmon.StartInstance(os.Getenv("PIXELMON_INSTANCE_ID")); err != nil {
 		return "", err
 	}
 
-	if err := pixelmon.StartService(); err != nil {
+	if err := pixelmon.StartService(os.Getenv("PIXELMON_INSTANCE_ID"), os.Getenv("PIXELMON_ZONE_ID"), ServerURL); err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf(":green_circle:   %s | Online", pixelmon.ServerURL), nil
+	return fmt.Sprintf(":green_circle:   %s | Online", ServerURL), nil
 }
 
 func stop(i *discordgo.Interaction) (string, error) {
 	log.Println("/pixelmon stop")
 
-	if err := SendDeferredMessage(i.AppID, i.Token, fmt.Sprintf(":red_square:   %s | Stopping the Pixelmon server", pixelmon.ServerURL)); err != nil {
+	// Check if user has correct role
+	verified := false
+	for _, role := range i.Member.Roles {
+		if role == os.Getenv("PIXELMON_ROLE_ID") {
+			verified = true
+			break
+		}
+	}
+	if !verified {
+		return "You don't have the required role to use this command!", nil
+	}
+
+	// Check if service is already stopped
+	status, _, err := pixelmon.GetStatus(ServerURL)
+	if err != nil {
+		return "", err
+	}
+	if !status {
+		return fmt.Sprintf(":red_circle:   %s | Offline", ServerURL), nil
+	}
+
+	if err := SendDeferredMessage(i.AppID, i.Token, fmt.Sprintf(":red_square:   %s | Stopping the Pixelmon server", ServerURL)); err != nil {
 		return "", err
 	}
 
-	if err := pixelmon.StopInstance(); err != nil {
+	if err := pixelmon.StopService(os.Getenv("PIXELMON_INSTANCE_ID"), os.Getenv("PIXELMON_ZONE_ID"), ServerURL, os.Getenv("PIXELMON_RCON_PASSWORD")); err != nil {
 		return "", err
 	}
 
-	if err := pixelmon.StopService(); err != nil {
+	if err := pixelmon.StopInstance(os.Getenv("PIXELMON_INSTANCE_ID")); err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf(":red_circle:   %s | Offline", pixelmon.ServerURL), nil
+	return fmt.Sprintf(":red_circle:   %s | Offline", ServerURL), nil
 }
 
 func say(i *discordgo.Interaction) (string, error) {
+	log.Printf("/pixelmon say")
+
+	// Check if service is already stopped
+	status, _, err := pixelmon.GetStatus(ServerURL)
+	if err != nil {
+		return "", err
+	}
+	if !status {
+		return fmt.Sprintf(":red_circle:   %s | Offline", ServerURL), nil
+	}
+
+	// Say message to server
+	message := i.ApplicationCommandData().Options[0].Options[0].StringValue()
+	if err := pixelmon.SayMessage(os.Getenv("PIXELMON_INSTANCE_ID"), os.Getenv("PIXELMON_RCON_PASSWORD"), i.Member.Nick, message); err != nil {
+		return "", err
+	}
+
 	return "", nil
 }
 
 func whitelist(i *discordgo.Interaction) (string, error) {
+	log.Printf("/pixelmon whitelist")
+
+	// Check if user has correct role
+	verified := false
+	for _, role := range i.Member.Roles {
+		if role == os.Getenv("PIXELMON_ROLE_ID") {
+			verified = true
+			break
+		}
+	}
+	if !verified {
+		return "You don't have the required role to use this command!", nil
+	}
+
+	// Check if service is already stopped
+	status, _, err := pixelmon.GetStatus(ServerURL)
+	if err != nil {
+		return "", err
+	}
+	if !status {
+		return fmt.Sprintf(":red_circle:   %s | Offline", ServerURL), nil
+	}
+
+	// Add username to whitelist
+	username := i.ApplicationCommandData().Options[0].Options[0].StringValue()
+	if err := pixelmon.AddToWhitelist(os.Getenv("PIXELMON_INSTANCE_ID"), os.Getenv("PIXELMON_RCON_PASSWORD"), username); err != nil {
+		return "", err
+	}
+
 	return "", nil
 }
--- internal/pixelmon/pixelmon.go
+++ internal/pixelmon/pixelmon.go
@@ -1,27 +1,253 @@
 package pixelmon
 
 import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+
 	"github.com/kn-lim/dreamingway-bot/internal/mcstatus"
 )
 
-func GetStatus() (bool, int, error) {
-	// log.Println("GetStatus()")
-
-	return mcstatus.GetMCStatus(ServerURL)
+func GetStatus(url string) (bool, int, error) {
+	return mcstatus.GetMCStatus(url)
 }
 
-func StartInstance() error {
+func StartInstance(instanceID string) error {
+	// Setup AWS session
+	cfg, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	// Get EC2 instance
+	client, instance, err := getInstance(cfg, instanceID)
+	if err != nil {
+		return err
+	}
+
+	// Start EC2 instance
+	if instance.State.Name == "stopped" {
+		input := &ec2.StartInstancesInput{
+			InstanceIds: []string{*instance.InstanceId},
+		}
+
+		_, err := client.StartInstances(context.TODO(), input)
+		if err != nil {
+			log.Printf("Error! Failed to start instance: %v", err)
+			return err
+		}
+	}
+
 	return nil
 }
 
-func StartService() error {
+func StartService(instanceID string, zoneID string, url string) error {
+	// Setup AWS session
+	cfg, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	// Get EC2 instance
+	_, instance, err := getInstance(cfg, instanceID)
+	if err != nil {
+		return err
+	}
+
+	// Wait till EC2 instance is running
+	for {
+		if instance.State.Name == "running" {
+			break
+		}
+
+		log.Println("Waiting for instance...")
+		time.Sleep(delay * time.Second)
+	}
+
+	log.Println("Instance is running")
+
+	// Create Route53 DNS entry
+	if err := createInstanceDNSEntry(cfg, instance, zoneID, url); err != nil {
+		return err
+	}
+
+	// Send start command to EC2 instance
+	client := ssm.NewFromConfig(cfg)
+	documentName := "AWS-RunShellScript"
+	params := map[string][]string{
+		"commands": {"cd /opt/pixelmon/ && tmux new-session -d -s minecraft './start.sh'"},
+	}
+	input := &ssm.SendCommandInput{
+		InstanceIds:  []string{instanceID},
+		DocumentName: &documentName,
+		Parameters:   params,
+	}
+	_, err = client.SendCommand(context.TODO(), input)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Sent command to EC2 instance")
+
+	// Check if service is online
+	for {
+		status, _, err := GetStatus(url)
+		if err != nil {
+			return err
+		}
+
+		if status {
+			break
+		}
+
+		log.Println("Waiting for service...")
+		time.Sleep(delay * time.Second)
+	}
+
+	log.Println("Service is running")
+
 	return nil
 }
 
-func StopInstance() error {
+func StopInstance(instanceID string) error {
+	// Setup AWS session
+	cfg, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	// Get EC2 instance
+	client, instance, err := getInstance(cfg, instanceID)
+	if err != nil {
+		return err
+	}
+
+	// Stop EC2 instance
+	if instance.State.Name == "running" {
+		input := &ec2.StopInstancesInput{
+			InstanceIds: []string{*instance.InstanceId},
+		}
+
+		_, err := client.StopInstances(context.TODO(), input)
+		if err != nil {
+			log.Printf("Error! Failed to stop instance: %v", err)
+			return err
+		}
+	}
+
+	log.Println("Instance is stopped")
+
 	return nil
 }
 
-func StopService() error {
+func StopService(instanceID string, zoneID string, url string, rcon string) error {
+	// Setup AWS session
+	cfg, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	// Get EC2 instance
+	_, instance, err := getInstance(cfg, instanceID)
+	if err != nil {
+		return err
+	}
+
+	// Remove Route53 DNS entry
+	if err := removeInstanceDNSEntry(cfg, instance, zoneID, url); err != nil {
+		return err
+	}
+
+	// Send stop command to EC2 instance
+	client := ssm.NewFromConfig(cfg)
+	documentName := "AWS-RunShellScript"
+	params := map[string][]string{
+		"commands": {"mcrcon -H localhost -p " + rcon + " \"stop\""},
+	}
+	input := &ssm.SendCommandInput{
+		InstanceIds:  []string{instanceID},
+		DocumentName: &documentName,
+		Parameters:   params,
+	}
+	_, err = client.SendCommand(context.TODO(), input)
+	if err != nil {
+		log.Printf("Error! Couldn't send command: %v", err)
+		return err
+	}
+
+	log.Println("Sent command to EC2 instance")
+
+	// Check if service is offline
+	for {
+		log.Println("Waiting for service...")
+		time.Sleep(delay * time.Second)
+
+		status, _, err := GetStatus(url)
+		if err != nil {
+			return err
+		}
+
+		if !status {
+			break
+		}
+	}
+
+	log.Println("Service is stopped")
+
+	return nil
+}
+
+func SayMessage(instanceID string, rcon string, username string, message string) error {
+	cfg, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	// Send say command to Pixelmon EC2 instance
+	client := ssm.NewFromConfig(cfg)
+	documentName := "AWS-RunShellScript"
+	params := map[string][]string{
+		"commands": {"mcrcon -H localhost -p " + rcon + " \"say " + username + ": " + message + "\""},
+	}
+	input := &ssm.SendCommandInput{
+		InstanceIds:  []string{instanceID},
+		DocumentName: &documentName,
+		Parameters:   params,
+	}
+	_, err = client.SendCommand(context.TODO(), input)
+	if err != nil {
+		log.Printf("Error! Couldn't send command: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func AddToWhitelist(instanceID string, rcon string, username string) error {
+	cfg, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	// Send whitelist command to Pixelmon EC2 instance
+	client := ssm.NewFromConfig(cfg)
+	documentName := "AWS-RunShellScript"
+	params := map[string][]string{
+		"commands": {"mcrcon -H localhost -p " + rcon + " \"whitelist add " + username + "\""},
+	}
+	input := &ssm.SendCommandInput{
+		InstanceIds:  []string{instanceID},
+		DocumentName: &documentName,
+		Parameters:   params,
+	}
+	_, err = client.SendCommand(context.TODO(), input)
+	if err != nil {
+		log.Printf("Error! Couldn't send command: %v", err)
+		return err
+	}
+
 	return nil
 }
